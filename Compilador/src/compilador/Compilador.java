578a78fa21a1021ddc403b5709a9de13c5185512
chuckintosh <carlos.srmzl@gmail.com>
2015-03-17 13:56:47 -0700

    Update Compilador.java

--- Compilador/src/compilador/Compilador.java
+++ Compilador/src/compilador/Compilador.java
@@ -15,7 +15,7 @@ public class Compilador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Adios Mundo");
     }
     
 }
